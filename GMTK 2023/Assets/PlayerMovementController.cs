7465da95255cf891288b621fdc6415fe03a09e5f
JasonReff <jmr8382@rit.edu>
2023-07-07 20:56:57 -0400

    Reset Project

--- GMTK 2023/Assets/PlayerMovementController.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public class PlayerMovementController : MovementController
-{
-    private void Update()
-    {
-        Move(new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")));
-    }
-}
