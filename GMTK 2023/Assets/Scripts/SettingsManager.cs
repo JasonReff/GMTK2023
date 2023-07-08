9c15c963395162adbb44afe35e7e24c6a92e8fec
JasonReff <jmr8382@rit.edu>
2023-07-08 19:25:15 -0400

    Add settings manager

---
+++ GMTK 2023/Assets/Scripts/SettingsManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SettingsManager : MonoBehaviour
+{
+    [SerializeField] private Slider _musicSlider, _effectsSlider, _voicesSlider, _masterVolumeSlider;
+    [SerializeField] private List<AudioClip> _effectsClips, _voiceClips;
+
+    private void Start()
+    {
+        _musicSlider.value = AudioManager.MusicVolume();
+        _effectsSlider.value = AudioManager.EffectsVolume();
+        _voicesSlider.value = AudioManager.VoiceVolume();
+        _masterVolumeSlider.value = AudioManager.MasterVolume();
+    }
+
+    public void SetMusicVolume(float value)
+    {
+        AudioManager.SetMusicVolume(value);
+    }
+
+    public void SetEffectsVolume(float value)
+    {
+        AudioManager.SetEffectsVolume(value);
+    }
+
+    public void SetVoicesVolume(float value)
+    {
+        AudioManager.SetVoicesVolume(value);
+    }
+
+    public void SetMasterVolume(float value)
+    {
+        AudioManager.SetMasterVolume(value);
+    }
+
+    public void PlayEffectsClip()
+    {
+        AudioManager.PlaySoundEffect(_effectsClips.Rand());
+    }
+
+    public void PlayVoiceClip()
+    {
+        AudioManager.PlayVoiceLine(_voiceClips.Rand());
+    }
+}
