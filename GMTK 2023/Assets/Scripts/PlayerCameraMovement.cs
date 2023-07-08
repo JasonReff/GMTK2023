3df3af84f0a23e4e712bdf905fde38016273c59e
JasonReff <jmr8382@rit.edu>
2023-07-08 14:11:01 -0400

    Camera bounds

--- GMTK 2023/Assets/Scripts/PlayerCameraMovement.cs
+++ GMTK 2023/Assets/Scripts/PlayerCameraMovement.cs
@@ -6,6 +6,7 @@ public class PlayerCameraMovement : MonoBehaviour
 {
     [SerializeField] private Camera _camera;
     [SerializeField] private float _movementSpeed = 1f, _cameraMovementSpeed = 0.1f;
+    [SerializeField] private float _lowestBound, _highestBound, _leftestBound, _rightestBound;
 
     private void Update()
     {
@@ -14,7 +15,23 @@ public class PlayerCameraMovement : MonoBehaviour
 
     private void MovePosition(Vector2 movement)
     {
-        transform.localPosition = (Vector2)transform.localPosition + movement;
-        //_camera.transform.localPosition = (Vector2)_camera.transform.localPosition + movement;
+        var newPosition = (Vector2)transform.localPosition + movement;
+        if (newPosition.y < _lowestBound)
+        {
+            newPosition.y = _lowestBound;
+        }
+        if (newPosition.y > _highestBound)
+        {
+            newPosition.y = _highestBound;
+        }
+        if (newPosition.x < _leftestBound)
+        {
+            newPosition.x = _leftestBound;
+        }
+        if (newPosition.x > _rightestBound)
+        {
+            newPosition.x = _rightestBound;
+        }
+        transform.localPosition = newPosition;
     }
 }
