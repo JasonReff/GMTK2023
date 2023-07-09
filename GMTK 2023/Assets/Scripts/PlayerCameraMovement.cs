1770a0789abdd17fbce68d41b3a8033a1461233d
JasonReff <jmr8382@rit.edu>
2023-07-09 09:58:49 -0400

    Change build settings

--- GMTK 2023/Assets/Scripts/PlayerCameraMovement.cs
+++ GMTK 2023/Assets/Scripts/PlayerCameraMovement.cs
@@ -1,17 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class PlayerCameraMovement : MonoBehaviour
 {
     [SerializeField] private Camera _camera;
-    [SerializeField] private float _movementSpeed = 1f, _cameraMovementSpeed = 0.1f, _driftSpeed = 0.01f;
+    [SerializeField] private float _movementSpeed = 1f, _cameraMovementSpeed = 0.1f, _driftSpeed = 0.01f, _shakeRate = 0.2f, _shakeStrength = 1f;
     [SerializeField] private float _lowestBound, _highestBound, _leftestBound, _rightestBound;
+    private float _shakeTimer = 0f;
 
     private void Update()
     {
         MovePosition(new Vector2(Input.GetAxisRaw("Horizontal") * _movementSpeed * Time.deltaTime, Input.GetAxisRaw("Vertical") * _movementSpeed * Time.deltaTime));
         CameraDrift();
+        _shakeTimer += Time.deltaTime;
+        if (_shakeTimer > _shakeRate)
+        {
+            _shakeTimer = 0;
+            CameraShake();
+        }
     }
 
     private void MovePosition(Vector2 movement)
@@ -39,5 +47,10 @@ public class PlayerCameraMovement : MonoBehaviour
     private void CameraDrift()
     {
         transform.localPosition = transform.localPosition - transform.localPosition * _driftSpeed * Time.deltaTime;
+    }
+
+    private void CameraShake()
+    {
+        transform.DOShakePosition(0.1f, _shakeStrength);
     }
 }
