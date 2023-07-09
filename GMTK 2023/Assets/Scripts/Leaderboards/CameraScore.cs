0b7c3caa18513f3466bf887757135f342d195728
JasonReff <jmr8382@rit.edu>
2023-07-08 22:13:49 -0400

    Score and Combo

--- GMTK 2023/Assets/Scripts/Leaderboards/CameraScore.cs
+++ GMTK 2023/Assets/Scripts/Leaderboards/CameraScore.cs
@@ -1,38 +1,126 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
 
 public class CameraScore : MonoBehaviour
 {
     [SerializeField] private ScoreManager _scoreManager;
     [SerializeField] private CameraZoom _zoom;
     [SerializeField] private BoxCollider2D _collider;
-    [SerializeField] private float _scorePerSecond, _trickMultiplier, _focusMultiplier, _zoomMultiplier;
+    [SerializeField] private float _scorePerSecond, _focusMultiplier, _zoomMultiplier;
+    [SerializeField] private TextMeshProUGUI _zoomTextbox, _focusTextbox, _frameTextbox, _comboTextbox;
+    private float _comboTimer, _comboScore;
+    private int _overlappingColliders = 0;
+    private bool _isComboActive, _skaterInFocus;
+    private Coroutine _comboEnd;
+
+    private void Update()
+    {
+        CalculateScore();
+        if (_isComboActive && _comboScore >= 100)
+        {
+            _comboTextbox.text = $"Combo: {(int)_comboScore}";
+        }
+    }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.TryGetComponent(out Skater skater))
         {
-            ScorePerFrame(skater);
+            RateSkater(skater);
+        }
+        else
+        {
+            if (_isComboActive)
+                _comboEnd = StartCoroutine(EndComboCoroutine());
         }
     }
 
-    private void ScorePerFrame(Skater skater)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        var baseScore = _scorePerSecond * Time.deltaTime;
-        if (skater.IsDoingTrick)
+        if (collision.TryGetComponent(out SkaterPart part))
         {
-            baseScore *= _trickMultiplier;
+            _overlappingColliders++;
         }
-        if (skater.IsInFocus())
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out SkaterPart part))
+        {
+            if (_overlappingColliders > 0)
+                _overlappingColliders--;
+        }
+    }
+
+    private void CalculateScore()
+    {
+        if (_comboEnd != null)
+        {
+            StopCoroutine(_comboEnd);
+        }
+        _isComboActive = true;
+        var baseScore = _scorePerSecond * _overlappingColliders * Time.deltaTime;
+        switch (_overlappingColliders)
+        {
+            case 0:
+                _frameTextbox.text = "Framing: Lame";
+                _frameTextbox.color = Color.red;
+                break;
+            case 1:
+                _frameTextbox.text = "Framing: OK";
+                _frameTextbox.color = new Color(255, 165, 0);
+                break;
+            case 2:
+                _frameTextbox.text = "Framing: Nice";
+                _frameTextbox.color = Color.yellow;
+                break;
+            case 3:
+                _frameTextbox.text = "Framing: Radical";
+                _frameTextbox.color = Color.green;
+                break;
+            default:
+                break;
+        }
+        if (_skaterInFocus)
         {
             baseScore *= _focusMultiplier;
+            _focusTextbox.text = "Focus: Crisp";
+            _focusTextbox.color = Color.green;
+        }
+        else
+        {
+            _focusTextbox.text = "Focus: Whack";
+            _focusTextbox.color = Color.red;
         }
         baseScore *= _zoom.ZoomLevel() * _zoomMultiplier;
-        //factor in overlap
-        AddScore(baseScore);
+        if (_zoom.ZoomLevel() * _zoomMultiplier > 1)
+        {
+            _zoomTextbox.text = "Zoom: Tight";
+            _zoomTextbox.color = Color.green;
+        }
+        else
+        {
+            _zoomTextbox.text = "Zoom: Far Out";
+            _zoomTextbox.color = Color.red;
+        }
+        _comboScore += baseScore;
+    }
+
+    private void RateSkater(Skater skater)
+    {
+        _skaterInFocus = skater.IsInFocus();
     }
 
     private void AddScore(float score)
     {
         _scoreManager.AddScore(score);
+    }
+
+    private IEnumerator EndComboCoroutine()
+    {
+        _isComboActive = false;
+        yield return new WaitForSeconds(_comboTimer);
+
     }
 }
---
+++ GMTK 2023/Assets/SkaterPart.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkaterPart : MonoBehaviour
+{
+    [SerializeField] private int _points;
+    public int Points { get => _points; }
+}
